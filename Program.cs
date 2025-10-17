c06914076a2fca40ad6b3bd95670c1eb03cbab0b
skvortsov-ivan <ivan.skvortsov@chasacademy.se>
2025-10-17 10:21:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Chessboard2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
